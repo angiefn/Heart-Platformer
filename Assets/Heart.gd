47e2d1b430f56628758427173c2dad19dd89d149
titran3 <121971162+titran3@users.noreply.github.com>
2024-02-15 14:24:32 -0800

    added levle completed

--- Assets/Heart.gd
+++ Assets/Heart.gd
@@ -4,4 +4,4 @@ func _on_body_entered(body):
 	queue_free()
 	var hearts = get_tree().get_nodes_in_group("Hearts")
 	if hearts.size() == 1:
-		print("Level Completed")
+		Events.level_completed.emit()
--- Assets/world.gd
+++ Assets/world.gd
@@ -2,8 +2,12 @@ extends Node2D
 
 @onready var polygon_2D = $StaticBody2D/CollisionPolygon2D/Polygon2D
 @onready var collision_polygon_2d = $StaticBody2D/CollisionPolygon2D
+@onready var level_completed = $"CanvasLayer/Level Completed"
 
 func _ready():
 	RenderingServer.set_default_clear_color(Color.BLACK)
 	polygon_2D.polygon = collision_polygon_2d.polygon
+	Events.level_completed.connect(show_level_completed)
 	
+func show_level_completed():
+	level_completed.show()
