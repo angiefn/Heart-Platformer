4f834c27126ac974facdda23f973af99545d5915
titran3 <121971162+titran3@users.noreply.github.com>
2024-02-25 20:14:44 -0800

    p12

--- Assets/start_menu.gd
+++ Assets/start_menu.gd
@@ -3,6 +3,7 @@ extends CenterContainer
 @onready var start_game_button = $VBoxContainer/StartGameButton
 
 func _ready():
+	RenderingServer.set_default_clear_color(Color.BLACK)
 	start_game_button.grab_focus()
 
 func _on_quit_button_pressed():
--- Assets/world.gd
+++ Assets/world.gd
@@ -12,6 +12,7 @@ func show_level_completed():
 	level_completed.show()
 	if not next_level is PackedScene: return
 	get_tree().paused = true
+	await get_tree().create_timer(1.0).timeout
 	await LevelTransition.fade_to_black()
 	get_tree().paused = false
 	get_tree().change_scene_to_packed(next_level)
